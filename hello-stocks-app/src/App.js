f4ad905ab577ceee7bd45dd1c52fa8b933b01ace
sjohnson96 <johnscott10k@gmail.com>
2021-05-18 17:20:52 -0400

    Got ticker search function working

--- hello-stocks-app/src/App.js
+++ hello-stocks-app/src/App.js
@@ -4,8 +4,6 @@ import StockSearch from './components/StockSearch';
 function App() {
   return (
     <div className="App">
-      <h2>Does this work?</h2>
-      <h1>Test Test</h1>
       <StockSearch />
     </div>
   );
--- hello-stocks-app/src/components/StockSearch.js
+++ hello-stocks-app/src/components/StockSearch.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect,  useContext} from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import axios from 'axios';
+import { formatIStocksSnapshotTickerRaw } from "@polygon.io/client-js/lib/rest/stocks/snapshots";
 
 const StockSearch = ( ) => {
+
+  const [tickers, setTickers] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    if (search === '') {
+      return;
+    }
+    axios.get(`https://api.polygon.io/v3/reference/tickers?market=stocks&search=${search}&active=true&sort=ticker&order=asc&limit=10&apiKey=vHjNP5FWBDFMkOyTytTHerS_1MYNXG5z`)
+      .then(res => {
+        if (res.data.results) {
+          setTickers(res.data.results);
+        }
+      })
+  }, [search])
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value);
+  }
+
   return (
     <div>
       <h2>Does this work? Test</h2>
+      <Autocomplete
+      id="combo-box-demo"
+      options={tickers}
+      getOptionLabel={(option) => option.ticker}
+      style={{ width: 300 }}
+      onInputChange={handleChange}
+      renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+    />
     </div>
   )
 }
